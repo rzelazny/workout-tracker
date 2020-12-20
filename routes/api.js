6708eb77b7a01fda57c8ce889f04557515575734
Ryan Zelazny <RyanZelazny@gmail.com>
2020-12-20 17:00:53 -0500

    adding new model

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
+const Workout = require("../models");
 
 router.post("/api/transaction", ({ body }, res) => {
     Workout.create(body)
