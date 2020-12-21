0cfa5c566e8caed17a4a3f4f3932cf98a3493fa1
Ryan Zelazny <RyanZelazny@gmail.com>
2020-12-21 18:13:55 -0500

    adding exercise reference to workout schema

--- models/workout.js
+++ models/workout.js
@@ -3,19 +3,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: "Enter a name for transaction"
-    },
-    value: {
-        type: Number,
-        required: "Enter an amount"
-    },
     date: {
         type: Date,
         default: Date.now
-    }
+    },
+    exercises: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "exercise"
+        }
+    ]
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
