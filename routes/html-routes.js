9e3471857a8fceffdab98bdeb1119c4fc7a7ec7d
Ryan Zelazny <RyanZelazny@gmail.com>
2020-12-21 17:00:42 -0500

    adding basic html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
 var path = require("path");
 
+//go home, default path
+router.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+//path for getting exercise data
 router.get("/exercise", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+//path for displaying stats
+router.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 module.exports = router;
