516fd3bd7e01b87160586bad72a7e12bab6df20f
Ryan Zelazny <RyanZelazny@gmail.com>
2020-12-21 21:05:51 -0500

    adding api/workours/range route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -35,7 +35,6 @@ router.put("/api/workouts/:id", (req, res) => {
     console.log("api PUT workouts running");
     console.log(req.params);
     db.Exercise.create(req.body)
-    console.log(req.body.duration)
         .then(({ _id }) => db.Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { exercises: _id }, $inc: {totalDuration: req.body.duration}}, { new: true }))
         .then(dbWorkout => {
             res.json(dbWorkout);
@@ -45,5 +44,22 @@ router.put("/api/workouts/:id", (req, res) => {
             res.status(400).json(err);
         });
 });
+
+
+//Gets stats for all workouts for charting
+router.get("/api/workouts/range", (req, res) => {
+    console.log("api GET range running");
+    db.Workout.find({})
+        .sort({ date: 1 })
+        .populate("exercises")
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
+
 
 module.exports = router;
